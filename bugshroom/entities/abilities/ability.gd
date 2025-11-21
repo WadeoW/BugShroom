73bf4b8c9be0b609098f87a4a1881e6f9b7f1ec1
WadeoW <wadewatson04@gmail.com>
2025-11-21 13:03:33 -0800

    worked on spore ring

---
+++ bugshroom/entities/abilities/ability.gd
@@ -0,0 +1,47 @@
+extends Area3D
+
+@export var AbilType: Resource
+@export var abilDamage: int = 15
+@export var abilRadius: int = 3
+@export var despawnTime: int = 5
+
+
+@onready var lifetime: Timer = $Lifetime
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var player = get_parent()
+
+var bodies_in_area = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	collision_shape_3d.radius = abilRadius
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_lifetime_timeout() -> void:
+	queue_free()
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("bug"):
+		bodies_in_area.append(body)
+		
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body in bodies_in_area:
+		bodies_in_area.erase(body)
+
+
+func _on_damage_tick_timer_timeout() -> void:
+	var damage = abilDamage
+	for body in bodies_in_area:
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+	
