beb928056c3956050a794e74dbbb8dd5f67d0539
WadeoW <wadewatson04@gmail.com>
2026-02-12 01:19:36 -0800

    .

--- bugshroom/player/amanita/animation_controller.gd
+++ bugshroom/player/amanita/animation_controller.gd
@@ -10,16 +10,16 @@ func _physics_process(delta: float) -> void:
 
 	var idle = !player.velocity
 	
-	animation_tree.set("parameters/AnimationNodeStateMachine/run/blend_position", lerp(player.velocity.length(), 0.0, 0.2))
-	animation_tree.set("parameters/TimeScale/scale", clamp(lerp(0.0, player.velocity.length()/2, 1.0), 1, 2))
+	animation_tree.set("parameters/run/blend_position", lerp(player.velocity.length(), 0.0, 0.2))
+	#animation_tree.set("parameters/TimeScale/scale", clamp(lerp(0.0, player.velocity.length()/2, 1.0), 1, 2))
 
 	
 	if player.is_jumping == true:
-		animation_tree.set("parameters/AnimationNodeStateMachine/conditions/is_jumping", true)
-		animation_tree.set("parameters/AnimationNodeStateMachine/conditions/is_on_ground", false)
+		animation_tree.set("parameters/conditions/is_jumping", true)
+		animation_tree.set("parameters/conditions/is_on_ground", false)
 	else:
-		animation_tree.set("parameters/AnimationNodeStateMachine/conditions/is_jumping", false)
-		animation_tree.set("parameters/AnimationNodeStateMachine/conditions/is_on_ground", true)
+		animation_tree.set("parameters/conditions/is_jumping", false)
+		animation_tree.set("parameters/conditions/is_on_ground", true)
 		
 	
 	
